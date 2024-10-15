070ba99a8472003d5f6d905c4a062d70a28e6040
DipaliTalware <d.talware@gmail.com>
2024-10-15 16:36:16 +0200

    Adding css to ListTable Component

---
+++ frontend/src/components/ListTable.tsx
@@ -0,0 +1,18 @@
+import List from "./List.tsx";
+
+const ListTable =() =>{
+    return(<>
+        <div className="list-table-container mt-5">
+        <List/>
+        <List/>
+        <List/>
+        <List/>
+        <List/>
+        <List/>
+        <List/>
+        <List/>
+        <List/>
+        </div>
+    </>)
+}
+export default ListTable;
