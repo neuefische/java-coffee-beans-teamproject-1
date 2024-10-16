de3d9ce0040b103095c0faa84f09f5248d3a49a0
mariyamango <de.msomkova@gmail.com>
2024-10-16 15:44:58 +0200

    Added list of products with name and quantity

--- frontend/src/components/list-page/ProductsTable.tsx
+++ frontend/src/components/list-page/ProductsTable.tsx
@@ -1,14 +1,39 @@
-import {ListGroup} from "react-bootstrap";
+import {Button, Col, ListGroup, Row} from "react-bootstrap";
 
-function ProductsTable () {
+function ProductsTable() {
     return <>
         <ListGroup>
-            <ListGroup.Item>Tomato</ListGroup.Item>
-            <ListGroup.Item>Potato</ListGroup.Item>
-            <ListGroup.Item>Milk</ListGroup.Item>
-            <ListGroup.Item>Bread</ListGroup.Item>
-            <ListGroup.Item>Cake</ListGroup.Item>
+            <ListGroup.Item>
+                <Row className="align-items-center">
+                    <Col xs={8}>
+                        <strong>tomato</strong>
+                    </Col>
+                    <Col xs={4} className="text-end">
+                        <Button
+                            variant="outline-primary"
+                            size="sm"
+                        >
+                            -
+                        </Button>
+                        <span className="mx-2">2</span>
+                        <Button
+                            variant="outline-primary"
+                            size="sm"
+                        >
+                            +
+                        </Button>
+                        <Button
+                            variant="danger"
+                            size="sm"
+                            className="ms-2"
+                        >
+                            Delete
+                        </Button>
+                    </Col>
+                </Row>
+            </ListGroup.Item>
         </ListGroup>
+
     </>
 }
 
