3a13f7bac935146faed9711432be2a6a1973d5eb
sama-hh <samahajimuradovah@gmail.com>
2024-10-16 14:47:59 +0200

    list page

---
+++ frontend/src/components/home-page/ListTable.tsx
@@ -0,0 +1,16 @@
+import List from "./List.tsx";
+
+const listsArr = ["list 1", "list 2", "list 3", "list 4", "list 5", "list 6", "list 7", "list 8"];
+
+const ListTable = () => {
+    const lists = listsArr.map((list: string) => {
+        return <List key={list} list={list} />
+    })
+
+    return (<>
+        <div className="list-table-container mt-5">
+            {lists}
+        </div>
+    </>)
+}
+export default ListTable;
