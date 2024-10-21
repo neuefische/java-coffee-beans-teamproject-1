aec5d1a62d9629257b3e95031c0e50cdaa792f65
mariyamango <de.msomkova@gmail.com>
2024-10-21 14:40:00 +0200

    Added deleting list function

--- frontend/src/components/home-page/ListTable.tsx
+++ frontend/src/components/home-page/ListTable.tsx
@@ -1,17 +1,33 @@
 import List from "./List.tsx";
+import { useState } from "react";
 
-const listsArr = ["list 1", "list 2", "list 3", "list 4", "list 5", "list 6", "list 7", "list 8"];
-
+const listsArr = [
+    { id: "1", name: "list 1" },
+    { id: "2", name: "list 2" },
+    { id: "3", name: "list 3" },
+    { id: "4", name: "list 4" },
+    { id: "5", name: "list 5" },
+    { id: "6", name: "list 6" },
+    { id: "7", name: "list 7" },
+    { id: "8", name: "list 8" },
+];
 
 const ListTable = () => {
-    const lists = listsArr.map((list: string) => {
-        return <List key={list} list={list} />
-    })
+    const [lists, setLists] = useState(listsArr);
 
-    return (<>
-        <div className="list-table-container mt-5">
-            {lists}
-        </div>
-    </>)
-}
+    const removeList = (id: string) => {
+        setLists((prevLists) => prevLists.filter((list) => list.id !== id));
+    };
+
+    return (
+        <>
+            <div className="list-table-container mt-5">
+                {lists.map((list) => (
+                    <List key={list.id} list={list.name} id={list.id} onDelete={removeList} />
+                ))}
+            </div>
+        </>
+    );
+};
+
 export default ListTable;
