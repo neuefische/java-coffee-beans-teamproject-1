6dec19aa105d537c163f0519c8df5afefe49220a
mariyamango <de.msomkova@gmail.com>
2024-10-21 10:24:35 +0200

    Added authorization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,47 @@ import Footer from "./components/Footer.tsx";
 import {Route, Routes} from "react-router-dom";
 import Home from "./components/home-page/Home.tsx";
 import ListPage from "./components/list-page/ListPage.tsx";
+import axios, {AxiosError} from "axios";
+import {useEffect, useState} from "react";
 
 function App() {
+
+    const [username, setUsername] = useState("");
+
+    function login() {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+
+        window.open(host + '/oauth2/authorization/github', '_self')
+    }
+
+    const loadUser = () => {
+        axios.get('/api/auth/me')
+            .then(response => {
+                const usernameResponse = response.data;
+                setUsername(usernameResponse);
+            })
+            .catch((error: AxiosError) => {
+                console.log(error)
+            })
+    }
+
+    useEffect(() => {
+        loadUser();
+    }, []);
+
     return (
         <>
-            <div className="d-flex flex-column min-vh-100">
-                <Header/>
-                <Routes>
-                    <Route path="/" element={<Home/>} />
-                    <Route path="/list" element={<ListPage/>} />
-                </Routes>
-                <Footer/>
-            </div>
-
+            {username ? (
+                    <div className="d-flex flex-column min-vh-100">
+                        <Header/>
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/list" element={<ListPage/>}/>
+                        </Routes>
+                        <Footer/>
+                    </div>) :
+                <button onClick={login}>Login with GitHub</button>
+            }
         </>
     )
 }
